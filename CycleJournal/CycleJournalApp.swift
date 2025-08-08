56beb73e53d252ae1225c55917e8a706dd0a6ab6
AkitoAndo <28ww.lo.ol.ww28@gmail.com>
2025-08-08 18:20:09 +0900

    Initial Commit

---
+++ CycleJournal/CycleJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleJournalApp.swift
+//  CycleJournal
+//
+//  Created by Akito Ando on 2025/08/08.
+//
+
+import SwiftUI
+
+@main
+struct CycleJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleJournalTests/CycleJournalTests.swift
@@ -0,0 +1,16 @@
+//
+//  CycleJournalTests.swift
+//  CycleJournalTests
+//
+//  Created by Akito Ando on 2025/08/08.
+//
+
+import Testing
+
+struct CycleJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
