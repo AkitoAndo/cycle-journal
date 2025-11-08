c46cc33862ff743de96442d119b6da3e81b853df
Takeshi <takeshiogata1105@gmail.com>
2025-11-09 00:46:43 +0900

    Initial Commit

---
+++ Cycle/CycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleApp.swift
+//  Cycle
+//
+//  Created by Takeshi Ogata on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct CycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleTests/CycleTests.swift
@@ -0,0 +1,17 @@
+//
+//  CycleTests.swift
+//  CycleTests
+//
+//  Created by Takeshi Ogata on 2025/11/08.
+//
+
+import Testing
+@testable import Cycle
+
+struct CycleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
